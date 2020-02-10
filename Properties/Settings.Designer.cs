f0215b3e2d1969209f9fd69af44c7a8daf291bcb
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2020-02-11 00:09:58 +0100

    Project finished! Redone the settings saving, program no longer needs external temp file in which to store data. It can also save other data from options menu.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,54 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GameLess.Properties
-{
-
-
+namespace GameLess.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public int MaxPlayHours {
+            get {
+                return ((int)(this["MaxPlayHours"]));
+            }
+            set {
+                this["MaxPlayHours"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool DesktopNotifications {
+            get {
+                return ((bool)(this["DesktopNotifications"]));
+            }
+            set {
+                this["DesktopNotifications"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string PathToCSVFile {
+            get {
+                return ((string)(this["PathToCSVFile"]));
+            }
+            set {
+                this["PathToCSVFile"] = value;
             }
         }
     }
